fbcbb1845610155accc42ac2c16e0b4ae22c3082
maxime <maxitrompette@ymail.com>
2017-10-14 10:21:08 +0200

    tab 2 dim : repertoire contact java

--- src/cours/Main.java
+++ src/cours/Main.java
@@ -1,41 +1,66 @@
 package cours;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int maison[][] = {
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,6,0,0,0,0,0,0,0},
-			{0,0,0,0,0,4,5,2,0,0,0,0,0,0},
-			{0,0,0,0,4,5,5,5,2,0,0,0,0,0},
-			{0,0,0,4,5,5,5,5,5,2,0,0,0,0},
-			{0,0,4,5,5,5,5,5,5,5,2,0,0,0},
-			{0,4,5,5,5,5,5,5,5,5,5,2,0,0},
-			{0,0,1,0,0,0,0,0,7,7,1,0,0,0},
-			{0,0,1,0,0,0,0,0,7,7,1,0,0,0},
-			{0,0,1,0,0,0,0,0,7,7,1,0,0,0},
-			{0,0,1,0,0,3,3,0,7,7,1,0,0,0},
-			{0,0,1,0,0,3,3,0,7,7,1,0,0,0},
-			{0,0,1,0,0,3,3,0,7,7,1,0,0,0}
+		Scanner sc = new Scanner(System.in);
+		boolean leafRepertory = true;
+		
+		String repertory[][] = {
+			{
+				"maxime",
+				"12",
+				"maxitrompette@ymail.com",
+				"0769317415",
+				"true"
+			},
+			{
+				"dwaps",
+				"36",
+				"contact@dwaps.fr",
+				"0651279211",
+				"true"
+			},
+			{
+				"schtroumpfette",
+				"15",
+				"schtroupfette@gmail.com",
+				"0000000000",
+				"false"
+			}
 		};
 		
-		for (int[] row : maison ) {
-			for (int column : row) {
-				switch (column) {
-					case 0: System.out.print(" "); break;
-					case 1: System.out.print("||"); break;
-					case 2: System.out.print("\\"); break;
-					case 3: System.out.print("H"); break;
-					case 4: System.out.print("/"); break;
-					case 5: System.out.print("▄"); break;
-					case 6: System.out.print("▲"); break;
-					case 7: System.out.print(""); break;
+		while (leafRepertory) {
+			System.out.println("Qui cherchez-vous ?");
+			String prenom = sc.nextLine();
+			boolean found = false;
+			
+			for (String[] contact : repertory) {
+				for (String detail : contact) {
+					if (detail.equals(prenom)) {
+						found = true;
+						for (int i = 0; i < contact.length; i++) {
+							System.out.println(contact[i]);
+						}
+					}
 				}
 			}
-			System.out.println("");
+			
+			if (!found) {
+				System.out.println("désolé cette personne ne fait pas partie des contacts");
+			}
+			
+			System.out.println("souhaitez vous faire une autre recherche? [On]");
+			String reponse = sc.nextLine();
+			
+			if (reponse.equals("n")) {
+				leafRepertory = false;
+				System.out.println("fermez avec succès. à bientôt.");
+			}
 		}
-		System.out.println("c'est une maison");
+		
+		sc.close();
 	}
-
 }
